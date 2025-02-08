3c109ce6681954883f5afa4da19786a3fa565f1b
kn134646@gmail.com <kn134646@gmail.com>
2025-02-08 10:22:57 +0700

    Initial commit for Dino Evolution Runner

---
+++ Assets/Scripts/DinoController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class DinoController : MonoBehaviour
+{
+    public float speed = 5.0f;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        
+    }
+}
