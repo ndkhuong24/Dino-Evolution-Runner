1f690bce5635e78e688dfebb052ed66ab9320479
kn134646@gmail.com <kn134646@gmail.com>
2025-02-11 16:28:16 +0700

    add Ground Scroller and Player run

--- Assets/Scripts/DinoController.cs
+++ Assets/Scripts/DinoController.cs
@@ -1,17 +1,79 @@
+﻿using System;
+using System.Collections;
 using UnityEngine;
 
 public class DinoController : MonoBehaviour
 {
-    public float speed = 5.0f;
     private Rigidbody2D rb;
+    public float jumpForce = 10f;
+    private bool isGrounded = true;
+    private bool isDashing = false;
+    public Animator anim;
+
+    //public bool isFacingRight = true;
+    //public float move;
+    //public float speed = 4.0f;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>(); // Lấy Animator từ nhân vật
     }
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded && !isDashing)
+        {
+            Jump();
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && isGrounded)  // Nhấn Shift để Dash
+        {
+            Dash();
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftShift) && isGrounded)
+        {
+            isDashing = false;
+            anim.SetBool("isDashing", false);
+        }
+
+        //di chuyển nhân vật
+        //move = Input.GetAxisRaw("Horizontal");
+        //rb.linearVelocity = new Vector2(speed * move, rb.linearVelocity.y);
+        ////xoay nhân vật
+        //if (isFacingRight == true && move == -1)
+        //{
+        //    transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        //    isFacingRight = false;
+        //}
+        //else if (isFacingRight == false && move == 1)
+        //{
+        //    transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        //    isFacingRight = true;
+        //}
+    }
+
+    private void Dash()
+    {
+        isDashing = true;
+        //isGrounded = false;
+        anim.SetBool("isDashing", true);
+    }
+
+    private void Jump()
+    {
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        isGrounded = false;
+        anim.SetBool("isJumping", true); // Bật animation Jump
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Nếu chạm đất, có thể nhảy tiếp
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+            anim.SetBool("isJumping", false); // Khi chạm đất, quay lại animation Run
+        }
     }
 }
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public TextMeshProUGUI currentTimeText; // Text hiển thị thời gian hiện tại
+    public TextMeshProUGUI bestTimeText;    // Text hiển thị thời gian cao nhất
+
+    private int currentTime = 0;
+    private int bestTime = 0;
+    private float timeElapsed = 0f;
+    public float speedMultiplier = 5f; // Hệ số tăng tốc thời gian
+    //private bool isPlaying = false;
+
+    public GameObject[] grounds;
+    public float speedScroller = 5f;
+    private float groundWidth;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //Lấy thời gian cao nhất từ bộ nhớ(nếu có)
+        bestTime = PlayerPrefs.GetInt("BestTime", 0);
+        bestTimeText.text = bestTime.ToString("D5");
+
+        //Lấy chiều rộng của ground
+        groundWidth = grounds[0].GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (isPlaying)
+        //{
+        timeElapsed += Time.deltaTime * speedMultiplier; // Thời gian chạy nhanh hơn
+        currentTime = (int)timeElapsed; // Ép về số nguyên
+        currentTimeText.text = currentTime.ToString("D5"); // Hiển thị dạng 
+        //}
+
+        MoveGround();
+        speedScroller += Time.deltaTime * 0.1f;
+    }
+
+    private void MoveGround()
+    {
+        foreach (GameObject ground in grounds)
+        {
+            //Di chuyển sàn 
+            ground.transform.position += Vector3.left * speedScroller * Time.deltaTime;
+
+            //Nếu sàn trượt ra khỏi màn hình, đặt lại về vị trí phía sau
+            if (ground.transform.position.x < -groundWidth)
+            {
+                float newX = ground.transform.position.x + groundWidth * grounds.Length;
+                ground.transform.position = new Vector3(newX, ground.transform.position.y, ground.transform.position.z);
+            }
+        }
+    }
+
+    //public void StartGame()
+    //{
+    //    isPlaying = true;
+    //    currentTime = 0;
+    //}
+
+    //public void EndGame()
+    //{
+    //    isPlaying = false;
+
+    //    // Nếu thời gian hiện tại lớn hơn best time -> Cập nhật best time
+    //    if (currentTime > bestTime)
+    //    {
+    //        bestTime = currentTime;
+    //        PlayerPrefs.SetFloat("BestTime", bestTime);
+    //        PlayerPrefs.Save();
+    //        bestTimeText.text = "Best Time: " + bestTime.ToString("F2") + "s";
+    //    }
+    //}
+}
